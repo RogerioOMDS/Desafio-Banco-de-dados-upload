f8d066a26405abd219215e454f6777309c176145
Rogerio <r.otavioms@gmail.com>
2021-01-19 22:37:14 -0300

    Solved

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,32 @@
+import {Entity, Column, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn, ManyToOne, JoinColumn} from 'typeorm'
+import Category from '../models/Category'
+
+@Entity('transactions')
 class Transaction {
+
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({name: 'category_id'})
+  category: Category;
 
+  @Column()
+  category_id: string;
+  
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
